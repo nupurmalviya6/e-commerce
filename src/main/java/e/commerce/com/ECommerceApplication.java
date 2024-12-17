0bd5443dd18a2ca456ec9efe0bdb82dff40cae87
Nupur Malviya <nupurmalviya6@gmail.com>
2024-12-17 12:42:23 +0530

    initial setup

---
+++ src/main/java/e/commerce/com/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package e.commerce.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
