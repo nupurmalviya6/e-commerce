52c80b4fe1f72188ee0d5fe0f345794dd3b241c0
Jitendra Malviya <jeetumalviya484@gmail.com>
2024-12-19 20:04:26 +0530

    Conflict resolve

--- src/main/java/e/commerce/com/user/model/User.java
+++ src/main/java/e/commerce/com/user/model/User.java
@@ -6,8 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
